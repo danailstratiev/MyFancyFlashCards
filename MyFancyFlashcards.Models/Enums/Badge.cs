3cb343547f102dfc10911ab68d2f92c97c37e40e
danailstratirv <stratyd@abv.bg>
2019-11-19 00:42:13 +0200

    Enums added

---
+++ MyFancyFlashcards.Models/Enums/Badge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFancyFlashcards.Models.Enums
+{
+   public enum Badge
+    {
+        Rookie = 1,
+        Underdig = 2,
+        Experienced = 3,
+        Master = 4
+    }
+}
---
+++ MyFancyFlashcards.Models/Enums/Package.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFancyFlashcards.Models.Enums
+{
+   public enum Package
+    {
+        Basic = 1,
+        Advanced = 2,
+        Silver = 3,
+        Gold = 4
+    }
+}
